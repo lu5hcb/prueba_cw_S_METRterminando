d5d631c4d09b6558328dfc5a88b136111f7665a2
claudio <lu5hcb1234@gmail.com>
2019-08-31 04:13:10 +0200

    d

--- boton_rit_clarif.ino
+++ boton_rit_clarif.ino
@@ -4,7 +4,7 @@ void ritcla(void){
  if (clarificadori<1){
     delay(100); 
    lcd.setCursor(9,0);
-   lcd.print("     ");
+   lcd.print("  kk   ");
    lcd.setCursor(9,0);
    lcd.print(clarificadori);
  } 
@@ -13,9 +13,11 @@ void ritcla(void){
  if (clarificadori>1){
    lcd.setCursor(9,0);
    delay(99);
-   lcd.print("      ");
+   lcd.print("    ");
    lcd.setCursor(9,0);
    lcd.print(clarificadori*10);
  } 
 
   }
+
+  
